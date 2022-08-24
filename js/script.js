043e272f574343b83910d11a283141c6b103ad1c
Lights0n <lyadouskiy@gmail.com>
2022-08-24 23:52:04 +0300

    feat: deck html + somee js

--- js/script.js
+++ js/script.js
@@ -62,7 +62,6 @@ IogSothoth.addEventListener('click', () => {
   chosenAncient = 'iogSothoth'
   AncientName.textContent = 'Йог-Сотот';
   cardTracker()
-  IogSothoth.style.opacity = '1';
 })
 
 ShubNiggurath.addEventListener('click', () => {
@@ -70,3 +69,66 @@ ShubNiggurath.addEventListener('click', () => {
   AncientName.textContent = 'Шуб-Ниггурат';
   cardTracker()
 })
+
+// difficulties
+
+// difficulty-easy
+// difficulty-medium
+// difficulty-hard
+// difficulty-expta-hard
+
+const difficultyCnt = document.querySelector('.main__difficulty');
+const labels = document.getElementsByTagName("label")
+
+const difficultyExtraEasy = document.getElementById('difficulty-extra-easy');
+const difficultyEasy = document.getElementById('difficulty-easy');
+const difficultyMedium = document.getElementById('difficulty-medium');
+const difficultyHard = document.getElementById('difficulty-hard');
+const difficultyExtraHard = document.getElementById('difficulty-expta-hard');
+
+let myDifficulty
+// styles for chosen difficulty
+function activeDifficulty(chosen) {
+  for (let label of labels) {
+    label.classList.remove('active')
+
+    if (label.htmlFor === chosen.id) {
+      console.log(chosen.id);
+      label.classList.add('active')
+      myDifficulty = chosen.id
+      console.log(myDifficulty);
+    }
+  }
+}
+difficultyCnt.addEventListener('click', () => {
+  if (difficultyExtraEasy.checked) {
+    // console.log('ExtraEasy ON');
+    activeDifficulty(difficultyExtraEasy)
+  }
+  else if (difficultyEasy.checked) {
+    // console.log('difficultyEasy');
+    activeDifficulty(difficultyEasy)
+  }
+  else if (difficultyMedium.checked) {
+    // console.log('difficultyMedium');
+    activeDifficulty(difficultyMedium)
+  }
+  else if (difficultyHard.checked) {
+    // console.log('difficultyHard');
+    activeDifficulty(difficultyHard)
+  }
+  else if (difficultyExtraHard.checked) {
+    // console.log('difficultyExtraHard');
+    activeDifficulty(difficultyExtraHard)
+  }
+})
+
+// cards faces
+
+import cardsDataGreen from '../data/MythicCards/green/index.js'
+import cardsDataBrown from '../data/MythicCards/brown/index.js'
+import cardsDataBlue from '../data/MythicCards/blue/index.js'
+
+const currentCard = document.getElementById('current-card');
+
+currentCard.src = cardsDataBlue[8].cardFace
