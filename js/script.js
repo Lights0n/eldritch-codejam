3ffedea81c7f5db3b93bcac7fe7fe2d4dedf3060
lights0n <lyadouskiy@gmail.com>
2022-08-25 13:53:35 +0300

    feat: started JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ancientsData from '../data/ancients2.js'
-console.log(ancientsData);
 const Azatoth = document.getElementById('azathoth-card');
 const Cthulthu = document.getElementById('cthulthu-card');
 const IogSothoth = document.getElementById('iogSothoth-card');
@@ -18,18 +17,38 @@ const thSGreen = document.querySelector('.third-stage-green');
 const thSblue = document.querySelector('.third-stage-blue');
 const thSBrown = document.querySelector('.third-stage-brown');
 
+const copacity = document.querySelector('.copacity');
+const copacityGreenn = document.querySelector('.copacity-green');
+const copacityBrown = document.querySelector('.copacity-brown');
+const copacityBlue = document.querySelector('.copacity-blue');
+
+
 const AncientName = document.querySelector('.main__ancient-name');
+const ancients = document.querySelectorAll('.main__item');
 let chosenAncient;
-let difficulty;
+let difficulty
+let copacityVal
+let greenVal
+let brownVal
+let blueVal
+
 
 function cardTracker() {
+  for (let ancient of ancients) {
+    ancient.classList.remove('active-ancient')
+  }
+
   if (chosenAncient == 'azathoth') {
+    Azatoth.classList.add('active-ancient')
   }
   else if (chosenAncient == 'cthulhu') {
+    Cthulthu.classList.add('active-ancient')
   }
   else if (chosenAncient == 'iogSothoth') {
+    IogSothoth.classList.add('active-ancient')
   }
   else if (chosenAncient == 'shubNiggurath') {
+    ShubNiggurath.classList.add('active-ancient')
   }
   let ddd = ancientsData.map(el => {
     if (el.id == chosenAncient) {
@@ -40,9 +59,32 @@ function cardTracker() {
       sSblue.textContent = el.secondStage.blueCards
       sSBrown.textContent = el.secondStage.brownCards
       thSGreen.textContent = el.thirdStage.greenCards
-      thSblue.textContent = el.thirdStage.blueCards
       thSBrown.textContent = el.thirdStage.brownCards
+      thSblue.textContent = el.thirdStage.blueCards
     }
+
+    copacity.textContent = `Всего в колодах: ${+fSGreen.innerHTML
+      + +fSblue.innerHTML
+      + +fSBrown.innerHTML
+      + +sSGreen.innerHTML
+      + +sSblue.innerHTML
+      + +sSBrown.innerHTML
+      + +thSGreen.innerHTML
+      + +thSBrown.innerHTML
+      + +thSblue.innerHTML}
+      `
+    greenVal = +fSblue.innerHTML
+      + +sSblue.innerHTML
+      + +thSblue.innerHTML;
+    brownVal = +fSBrown.innerHTML
+      + +sSBrown.innerHTML
+      + +thSBrown.innerHTML
+    blueVal = +fSGreen.innerHTML
+      + +sSGreen.innerHTML
+      + +thSGreen.innerHTML
+    copacityGreenn.textContent = `Всего синих: ${greenVal}`
+    copacityBrown.textContent = `Всего каричневых: ${brownVal}`
+    copacityBlue.textContent = `Всего зеленых: ${blueVal}`
   })
 }
 
@@ -132,3 +174,24 @@ import cardsDataBlue from '../data/MythicCards/blue/index.js'
 const currentCard = document.getElementById('current-card');
 
 currentCard.src = cardsDataBlue[8].cardFace
+
+// decks main algritm
+console.log(cardsDataGreen);
+console.log(cardsDataBrown);
+console.log(cardsDataBlue);
+
+let extraEasyDeck
+let mediumDeck
+let arr = []
+
+function randomGenerate(array, end, start = 0) {
+  array = []
+  while (array.length < end) {
+    let el = Math.floor(Math.random() * 10);
+    if (array.includes(el)) continue;
+    else array.push(el)
+  }
+  return array
+}
+
+console.log(randomGenerate(arr, 5));
